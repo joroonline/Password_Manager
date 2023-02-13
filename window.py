e79930f98a3857eb7f5498d7b8e22509c1bc7958
Jonas <jonas.rot@tutanota.com>
2023-02-13 20:51:29 +0100

    + added button and text line in window.py

--- window.py
+++ window.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+# /usr/bin/env python3.10
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLineEdit, QLabel
+
 
 class MyWindow(QMainWindow):
     def __init__(self, width, height):
@@ -9,14 +11,27 @@ class MyWindow(QMainWindow):
 
         self.setGeometry(300, 300, width, height)
         self.setWindowTitle('My Window')
-        self.uiComponents()
+
+        self.button()
+        self.text_field()
 
         self.show()
 
-    def uiComponents(self):
+    def button(self):
         button = QPushButton('CLICK!', self)
-        button.setGeometry(300, 300, 100, 100)
-        button.clicked.connect(self.on_click())
+        button.setGeometry(100, 100, 100, 50)
+        button.clicked.connect(self.on_click)
+
+    def text_field(self):
+        self.textfield = QLineEdit(self)
+        self.textfield.setGeometry(300, 100, 200, 30)
 
     def on_click(self):
-        print('Button was clicked')
+        text = self.textfield.text()
+        print(f'Text: {text}')
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow(400, 400)
+    sys.exit(app.exec_())
