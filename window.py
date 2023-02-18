0445f0caf83ee7b3109da828024771e6ce916a70
Jonas <jonas.rot@tutanota.com>
2023-02-18 17:43:45 +0100

    window.py + getting information from dictionary  -> show when an argument given
    + create_button_list

--- window.py
+++ window.py
@@ -2,18 +2,23 @@
 # /usr/bin/env python3.10
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLineEdit, QLabel
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLineEdit, QComboBox, QListWidget, QListWidgetItem
 
 
 class MyWindow(QMainWindow):
-    def __init__(self, width, height):
+    def __init__(self, information: dict):
         super().__init__()
 
-        self.setGeometry(300, 300, width, height)
-        self.setWindowTitle('My Window')
+        if information.get('size') is not None:
+            width = information['size'][0]
+            height = information['size'][1]
+            self.setGeometry(300, 300, width, height)  # -> size
+            self.setWindowTitle('My Window')
 
-        self.button()
-        self.text_field()
+        #        self.button()
+        #        self.text_field()
+        if information.get('element') is not None:
+            self.create_button_list(information['element'])  # -> element
 
         self.show()
 
@@ -21,6 +26,15 @@ class MyWindow(QMainWindow):
         button = QPushButton('CLICK!', self)
         button.setGeometry(100, 100, 100, 50)
         button.clicked.connect(self.on_click)
+
+    def create_button_list(self, items):
+        self.list_widget = QListWidget(self)
+        self.list_widget.move(100, 50)
+        self.list_widget.resize(200, 150)
+
+        for item in items:
+            list_item = QListWidgetItem(item)
+            self.list_widget.addItem(list_item)
 
     def text_field(self):
         self.textfield = QLineEdit(self)
@@ -30,8 +44,15 @@ class MyWindow(QMainWindow):
         text = self.textfield.text()
         print(f'Text: {text}')
 
+    def button_clicked(self, element):
+        print(f'Button {element} wurde angeklickt')
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyWindow(400, 400)
+    information = {
+        'size': [400, 400],
+        'element': ['a', 'b', 'c']
+    }
+    window = MyWindow(information)
     sys.exit(app.exec_())
